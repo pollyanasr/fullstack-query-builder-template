5c39a02dbda57f81f710206c2cc90fe7bb8c0641
Pollyana Rodrigues <pollyanna.fla@gmail.com>
2025-05-20 09:23:30 -0300

    install knex.js

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World!" })
-})
+  response.json({ message: "Hello World!" });
+});
 
-app.listen(3333, () => console.log(`Server is running on port 3333`))
+app.listen(3000, () => console.log(`Server is running on port 3000`));
